fd04ef816f602a9c2152e0d64c587a05ee0ed98c
Dmitry Kirillov <dmitry@qunomedical.com>
2025-07-12 23:45:55 +0200

    re(draw)grow heart

--- heart.js
+++ heart.js
@@ -4,23 +4,23 @@ new p5((p5) => {
   let heartUv;
   let canvas;
   let pageSize = 1024;
-  let gridSize = 10;
-  let step = 0;
+  let gridSize = 20;
+  let step = 30;
   let noiseOffset = 0;
   
   p5.setup = () => {
     canvas = p5.createCanvas(pageSize, pageSize);
     p5.background(255);
-    p5.stroke(150);
+    p5.stroke(1);
     
-    heartUv = p5.loadImage('data/gptheart.jpg');
+    heartUv = p5.loadImage('data/gptheart2.jpg');
   }
   
   p5.draw = () => {
     step+=10;
     p5.background(255);
-    // preview image
-    // p5.image(heartUv, 0, 0, width, height);
+     //preview image
+     //p5.image(heartUv, 0, 0, width, height);
     p5.stroke(0);
     p5.noFill();
     p5.strokeWeight(1);
@@ -33,7 +33,9 @@ new p5((p5) => {
         let point1 = heartUv.get(x, y);
         
         if((point1[0] + point1[1] + point1[2]) > 15) {
-          points.push({ x, y })
+          points.push({ x, y });
+          points.push({ x: x-point1[1]/5, y: y-point1[1]/5 });
+          points.push({ x: x+point1[1]/5, y: y+point1[1]/5 });
         }
       }
     }
@@ -47,10 +49,10 @@ new p5((p5) => {
       
       const [r, g, b] = heartUv.get(anchor1.x, anchor1.y)
       const value = (r + g + b) / 3
-      const vector1 = P5.Vector.fromAngle(p5.radians(value) + p5.noise(anchor1.x + noiseOffset, anchor1.y + noiseOffset), 200)
+          const vector1 = P5.Vector.fromAngle(p5.radians(value) + p5.noise(anchor1.x + noiseOffset, anchor1.y + noiseOffset), 200)
+          p5.stroke((i+step)/20 % 200);
       p5.bezier(anchor1.x, anchor1.y, anchor1.x + vector1.x, anchor1.y + vector1.y, anchor2.x - vector1.x, anchor2.y - vector1.y, anchor2.x, anchor2.y);
     }
     noiseOffset += 0.01;
   }
 }, document.querySelector('main'));
-
